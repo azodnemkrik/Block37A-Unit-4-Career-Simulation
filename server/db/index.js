144ee45a9f2217837e6fac2531d8e2d354fc0789
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-23 07:12:49 -0500

    Initial Commit

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,13 @@ const client = require('./client')
 
 
 
-const seed = async()=> {
-  console.log("add logic to create and seed tables")
+const seed = async () => {
+	console.log("add logic to create and seed tables")
 };
 
 
 
 module.exports = {
-  client,
-  seed
+	client,
+	seed
 };
